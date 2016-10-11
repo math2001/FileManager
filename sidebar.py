be7118dcd89607ca5e352525bb554e92824008df
math2001 <mathieu.paturel@free.fr>
2016-10-11 17:20:16 +1100

    add duplicate cmd, update create cmd, formatting

--- sidebar.py
+++ sidebar.py
@@ -18,20 +18,46 @@ def log_path_in_status_bar(path):
 def quote(s):
 	return '"{}"'.format(s)
 
+def valid(*args):
+	return os.path.normpath(os.path.join(*args)).replace('/', os.path.sep)
+
+os.path.valid = valid
+
 class FmCreateCommand(sublime_plugin.TextCommand):
 	
 	def log_path_in_status_bar(self, name):
-		log_path_in_status_bar(os.path.join(self.path, name.replace('/', os.path.sep)))
+		log_path_in_status_bar(self.__get_path(name))
 
 	def create_file(self, name):
 		name = name.replace('/', os.path.sep)
-		path = os.path.join(self.path, name)
+		# path = os.path.join(self.path, name)
+		path = self.__get_path(name)
 		if os.path.isfile(path):
 			return self.window.open_file(path)
 		if not os.path.isdir(os.path.dirname(path)):
 			os.makedirs(os.path.dirname(path))
-		open(path, 'w').close()
-		self.window.open_file(path)
+		if name[-1] in ('/', os.path.sep):
+			os.makedirs(path)
+		else:
+			open(path, 'w').close()
+			self.window.open_file(path)
+
+	def __get_path(self, path):
+		if self.from_project:
+			path = path.split(' ')
+			try:
+				nb = int(path[0])
+			except ValueError:
+				base = self.project_data["folders"][0]['path']
+				path = ' '.join(path)
+			else:
+				base = self.project_data["folders"][nb]['path']
+				path = ' '.join(path[1:])
+				
+			return os.path.valid(base, path)
+		else:
+			return os.path.valid(self.path,path)
+			# return md(base, path); os.path.valid(base, path)
 
 
 	def run(self, edit, paths=[None], *args, **kwargs):
@@ -40,11 +66,26 @@ class FmCreateCommand(sublime_plugin.TextCommand):
 		self.settings = self.view.settings()
 
 		self.path = paths[0]
+
+		self.from_project = False
+
+		if self.path is None:
+			self.project_data = self.window.project_data()
+			if self.project_data:
+				self.path = self.project_data["folders"][0]['path']
+				self.from_project = True
+			else:
+				self.path = os.path.dirname(self.view.file_name())
+
+
 		if os.path.isfile(self.path):
 			self.path = os.path.dirname(self.path)
 
 
 		self.window.show_input_panel('New: ', '', self.create_file, self.log_path_in_status_bar, None)
+
+	def is_enabled(self, paths):
+		return len(paths) == 1
 
 
 class FmRenameCommand(sublime_plugin.TextCommand):
@@ -73,7 +114,7 @@ class FmRenameCommand(sublime_plugin.TextCommand):
 		self.settings = self.view.settings()
 
 		self.reopen = True
-		self.path = paths[0]
+		self.path = paths[0] or self.view.file_name()
 
 		if os.path.isdir(self.path):
 			self.reopen = False
@@ -91,6 +132,9 @@ class FmRenameCommand(sublime_plugin.TextCommand):
 		view.sel().clear()
 		view.sel().add(sublime.Region(0, len(os.path.splitext(basename)[0])))
 
+	def is_enabled(self, paths):
+		return len(paths) == 1
+
    
 class FmMoveCommand(sublime_plugin.TextCommand):
 	
@@ -99,7 +143,7 @@ class FmMoveCommand(sublime_plugin.TextCommand):
 
 	def move_file(self, path):
 		try:
-			os.makedirs(os.path.dirname(path), True)
+			os.makedirs(os.path.dirname(path))
 		except OSError:
 			pass
 		os.rename(self.path, path)
@@ -120,6 +164,44 @@ class FmMoveCommand(sublime_plugin.TextCommand):
 			self.log_path_in_status_bar, None)
 		view.sel().clear()
 		view.sel().add(sublime.Region( len(os.path.dirname(self.path)) + 1, len(self.path) - len(os.path.splitext(self.path)[1]) ))
+
+	def is_enabled(self, paths):
+		return len(paths) == 1
+
+class FmDuplicateCommand(sublime_plugin.TextCommand):
+	
+	def log_path_in_status_bar(self, path):
+		log_path_in_status_bar(path.replace('/', os.path.sep))
+
+	def duplicate(self, path):
+		if os.path.isfile(path):
+			return em('This file alredy exists!')
+		try:
+			os.makedirs(os.path.dirname(path))
+		except OSError:
+			pass
+		with open(self.path, 'r') as fp:
+			content = fp.read()
+		with open(path, 'w') as fp:
+			fp.write(content)
+
+		self.window.open_file(path)
+
+
+	def run(self, edit, paths=[None], *args, **kwargs):
+		self.window = self.view.window()
+		self.selection = sublime.Selection(self.view.id())
+		self.settings = self.view.settings()
+
+		self.path = paths[0] or self.view.file_name()
+
+		view = self.window.show_input_panel('Duplicate to: ', self.path, self.duplicate, 
+			self.log_path_in_status_bar, None)
+		view.sel().clear()
+		view.sel().add(sublime.Region( len(os.path.dirname(self.path)) + 1, len(self.path) - len(os.path.splitext(self.path)[1]) ))
+
+	def is_enabled(self, paths):
+		return len(paths) == 1
 
 
 class FmRevealCommand(sublime_plugin.TextCommand):
@@ -159,6 +241,3 @@ class FmDeleteCommand(sublime_plugin.TextCommand):
 			['Send item{} to trash'.format(('s' if len(self.paths) > 1 else ''))] + self.paths,
 			'Cancel'
 		], self.delete_file)
-
-
-
