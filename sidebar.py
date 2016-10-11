c595615354dbfcb6a640a4c77bb58a03270db187
math2001 <mathieu.paturel@free.fr>
2016-10-11 16:19:51 +1100

    add move and reveal feature

--- sidebar.py
+++ sidebar.py
@@ -18,7 +18,7 @@ def log_path_in_status_bar(path):
 def quote(s):
 	return '"{}"'.format(s)
 
-class FmCreateFileCommand(sublime_plugin.TextCommand):
+class FmCreateCommand(sublime_plugin.TextCommand):
 	
 	def log_path_in_status_bar(self, name):
 		log_path_in_status_bar(os.path.join(self.path, name.replace('/', os.path.sep)))
@@ -40,12 +40,14 @@ class FmCreateFileCommand(sublime_plugin.TextCommand):
 		self.settings = self.view.settings()
 
 		self.path = paths[0]
+		if os.path.isfile(self.path):
+			self.path = os.path.dirname(self.path)
 
-		self.window.show_input_panel('New file name: ', '', self.create_file, self.log_path_in_status_bar, None)
+
+		self.window.show_input_panel('New: ', '', self.create_file, self.log_path_in_status_bar, None)
 
 
-
-class FmRenameFileCommand(sublime_plugin.TextCommand):
+class FmRenameCommand(sublime_plugin.TextCommand):
 	
 	def log_path_in_status_bar(self, name):
 		log_path_in_status_bar(os.path.join(self.dirname, name.replace('/', os.path.sep)))
@@ -89,9 +91,54 @@ class FmRenameFileCommand(sublime_plugin.TextCommand):
 		view.sel().clear()
 		view.sel().add(sublime.Region(0, len(os.path.splitext(basename)[0])))
 
+   
+class FmMoveCommand(sublime_plugin.TextCommand):
+	
+	def log_path_in_status_bar(self, path):
+		log_path_in_status_bar(path.replace('/', os.path.sep))
+
+	def move_file(self, path):
+		try:
+			os.makedirs(os.path.dirname(path), True)
+		except OSError:
+			pass
+		os.rename(self.path, path)
+
+		if self.view.file_name() == self.path:
+			self.view.close()
+			self.window.open_file(path)
 
 
-class FmDeleteFileCommand(sublime_plugin.TextCommand):
+	def run(self, edit, paths=[None], *args, **kwargs):
+		self.window = self.view.window()
+		self.selection = sublime.Selection(self.view.id())
+		self.settings = self.view.settings()
+
+		self.path = paths[0] or self.view.file_name()
+
+		view = self.window.show_input_panel('New location: ', self.path, self.move_file, 
+			self.log_path_in_status_bar, None)
+		view.sel().clear()
+		view.sel().add(sublime.Region( len(os.path.dirname(self.path)) + 1, len(self.path) - len(os.path.splitext(self.path)[1]) ))
+
+
+class FmRevealCommand(sublime_plugin.TextCommand):
+	
+	def run(self, edit, paths=None, *args, **kwargs):
+		self.window = self.view.window()
+		self.selection = sublime.Selection(self.view.id())
+		self.settings = self.view.settings()
+
+		if paths is None:
+			paths = [self.view.file_name()]
+
+		for path in paths:
+			self.window.run_command("open_dir", { "dir": os.path.dirname(path), "file": os.path.basename(path) })
+
+
+
+
+class FmDeleteCommand(sublime_plugin.TextCommand):
 
 	def delete_file(self, index):
 		if index == 0:
@@ -112,3 +159,6 @@ class FmDeleteFileCommand(sublime_plugin.TextCommand):
 			['Send item{} to trash'.format(('s' if len(self.paths) > 1 else ''))] + self.paths,
 			'Cancel'
 		], self.delete_file)
+
+
+
