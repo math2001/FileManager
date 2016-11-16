24ea0fe585ca824a870f9fd3491ae3ccdbbfd2b4
math2001 <australie.p@gmail.com>
2016-11-17 08:17:18 +1100

    add _push-to-st2.py for easy dev

---
+++ _push-to-st2.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+"""
+    Push FileManager to ST2 packages directory.
+"""
+
+import shutil
+import os
+
+dst = 'C:\\Users\\math\\AppData\\Roaming\\Sublime Text 2\\Packages\\' + 'FileManager\\'
+
+
+def copy_and_overwrite(from_path, to_path):
+
+    def ignore(path, items):
+        return ['.git']
+
+    if os.path.exists(to_path):
+        shutil.rmtree(to_path)
+    shutil.copytree(from_path, to_path, ignore=ignore)
+
+if __name__ == '__main__':
+    copy_and_overwrite('.', dst)
+    print('Finish copying!')
