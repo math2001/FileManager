c026b9a1c41f569c6683c6cdaf3eeedd388e8dad
Daniel Dizdarevic <daniel.dizdarevic@itp1.uni-stuttgart.de>
2018-09-11 18:03:33 +0200

    Added missing import

--- FMcommands/duplicate.py
+++ FMcommands/duplicate.py
@@ -3,6 +3,7 @@ import shutil
 from ..sublimefunctions import *
 from ..input_for_path import InputForPath
 from .appcommand import AppCommand
+from ..send2trash import send2trash
 
 
 class FmDuplicateCommand(AppCommand):
