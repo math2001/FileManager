d7e4ff5944df4b661126a15c3df51bd85960fc84
Mathieu PATUREL <australie.p@gmail.com>
2020-01-05 22:54:55 +1100

    fix and improve fm commands is_visible method
    The logic use to be wrong with the menu_without_distraction (which
    honestly, no one should set to false anyway). It also now prints stuff
    to the console when it detects an internal error. I don't think this is
    the best way to report the error, but i don't want to nag the user
    with stupid pop ups, so i try to guess the best behavior when that
    happens.

--- FMcommands/appcommand.py
+++ FMcommands/appcommand.py
@@ -6,10 +6,26 @@ from ..sublimefunctions import get_settings, to_snake_case
 class AppCommand(sublime_plugin.ApplicationCommand):
     def is_visible(self, *args, **kwargs):
         settings = get_settings()
+        show = settings.get(
+            "show_" + to_snake_case(type(self).__name__.replace("Fm", ""))
+        )
+        if show is None:
+            # this should never happen, this is an error
+            # we could nag the user to get him to report that issue,
+            # but that's going to make this plugin really painful to use
+            # So, I just print something to the console, and hope someone
+            # sees and reports it
+            print(
+                "FileManager: No setting available for the command {!r}. This is an internal error, please report it".format(
+                    type(self).__name__
+                )
+            )
+            show = True
+
         return bool(
-            settings.get("show_" + to_snake_case(type(self).__name__.replace("Fm", "")))
+            show
             and (
-                self.is_enabled(*args, **kwargs)
-                or not settings.get("menu_without_distraction")
+                not settings.get("menu_without_distraction")
+                or self.is_enabled(*args, **kwargs)
             )
         )
