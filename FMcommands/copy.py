786711b1cf434058f835732296c70b6ccf441155
Mathieu PATUREL <australie.p@gmail.com>
2020-01-11 21:04:02 +1100

    copy: improve the copy submenu (relative path)
    root
      first
        second <- selected item
    
    relative path: first/second           use to be /first/second
    path from root: /first/second         [new]
    
    fix #55

--- FMcommands/copy.py
+++ FMcommands/copy.py
@@ -19,11 +19,15 @@ class FmCopyCommand(AppCommand):
                 text.append(os.path.basename(path))
             elif which == "absolute path":
                 text.append(path)
-            elif which == "relative path":
+            elif which in ("path from root", "relative path"):
                 for folder in folders:
                     if folder not in path:
                         continue
-                    text.append(path.replace(folder, ""))
+
+                    text.append(os.path.join("/", path.replace(folder, "")))
+                    if which == "relative path":
+                        # remove the initial /
+                        text[-1] = text[-1][1:]
                     break
 
         copy("\n".join(bit.replace(os.path.sep, "/") for bit in text))
