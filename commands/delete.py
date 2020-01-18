c71311306a2880b38442e4de39bd2ddb9ef41886
TheSecEng <zachary.schulze@gmail.com>
2020-01-17 21:26:58 -0500

    1. Updating the file to follow black formatting standards 2. Updated the Confirm Title, Subtitle, Cancel Title, Subtitle 3. Updated the paths_to_display to utilize `os.path.basename` vs `path.split()`

--- commands/delete.py
+++ commands/delete.py
@@ -14,10 +14,8 @@ class FmDeleteCommand(AppCommand):
                 try:
                     send2trash(path)
                 except OSError as e:
-                    sublime.error_message(
-                        "Unable to send to trash: {}".format(e))
-                    raise OSError(
-                        "Unable to send {0!r} to trash: {1}".format(path, e))
+                    sublime.error_message("Unable to send to trash: {}".format(e))
+                    raise OSError("Unable to send {0!r} to trash: {1}".format(path, e))
         if index > 1:
             view = self.window.find_open_file(self.paths[index - 2])
             if view is not None:
@@ -39,30 +37,23 @@ class FmDeleteCommand(AppCommand):
 
         self.paths = paths or [self.view.file_name()]
         if get_settings().get("ask_for_confirmation_on_delete") is not False:
-            confirm_text = "Send {0}item{1} to trash".format(
-                ("{0} ".format(
-                    len(self.paths)) if len(self.paths) > 1 else ""),
-                ("s" if len(
-                    self.paths) > 1 else "")
-            )
-            cancel_text = "Cancel deletion of {0}file{1}".format(
-                ("{0} ".format(
-                    len(self.paths)) if len(self.paths) > 1 else ""),
-                ("s" if len(
-                    self.paths) > 1 else "")
-            )
-            paths_to_display = [
-                ["Confirm", confirm_text],
-                ["Cancel", cancel_text]
-            ]
+            nitems = "{0} ".format(len(self.paths)) if len(self.paths) > 1 else ""
+            extras = "s" if len(self.paths) > 1 else ""
 
+            confirm_title = "Confirm"
+            confirm_subtitle = "Send {}item{} to trash".format(nitems, extras)
+            cancel_title = "Cancel All (Select an individual file to remove it from the deletion list)"
+            cancel_subtitle = "Cancel deletion of {0}item{1}".format(nitems, extras)
+
+            paths_to_display = [
+                [confirm_title, confirm_subtitle],
+                [cancel_title, cancel_subtitle],
+            ]
             for path in paths:
-                paths_to_display.append(
-                    [path.split(os.path.sep)[-1], path])
+                paths_to_display.append([os.path.basename(path), path])
 
             self.window.show_quick_panel(
-                paths_to_display,
-                self.delete,
+                paths_to_display, self.delete,
             )
         else:
             # index 0 is like clicking on the first option of the panel
