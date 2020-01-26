405391e3337b62b60dbbda9e5ee655b7188d0190
DeathAxe <deathaxe82@googlemail.com>
2020-01-26 10:05:17 +0100

    OS independent FmOpenInExplorer label
    This commit renames the label of the FmOpenInExplorer command to the
    default one used by ST: "Open Containing Folder..."
    
    This OS independent label doesn't require custom `platform` handling.

--- commands/open_in_explorer.py
+++ commands/open_in_explorer.py
@@ -1,13 +1,11 @@
 # -*- encoding: utf-8 -*-
 import os
 
-import sublime
-
 from .fmcommand import FmWindowCommand
 
 
 class FmOpenInExplorerCommand(FmWindowCommand):
-    def run(self, visible_on_platforms=None, paths=None):
+    def run(self, paths=None):
         # visible_on_platforms is just used by is_visible
         for path in paths or [self.window.active_view().file_name()]:
             if os.path.isdir(path):
@@ -17,8 +15,3 @@ class FmOpenInExplorerCommand(FmWindowCommand):
                 self.window.run_command(
                     "open_dir", {"dir": dirname, "file": basename},
                 )
-
-    def is_visible(self, visible_on_platforms=None, paths=None):
-        return (
-            visible_on_platforms is None or sublime.platform() in visible_on_platforms
-        ) and super().is_visible()
